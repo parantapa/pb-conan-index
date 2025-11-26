87f350b77b294d178ac6da3419910dda9e1004dd
Parantapa Bhattacharya <pb@parantapa.net>
2025-11-26 15:31:15 -0500

    Add random123

---
+++ recipes/random123/all/conanfile.py
@@ -0,0 +1,29 @@
+# type: ignore
+from conan import ConanFile
+from conan.tools.files import copy
+from conan.tools.scm import Git
+
+
+class Random123(ConanFile):
+    name = "random123"
+    version = "v1.14.0"
+    exports_sources = "include/*/*.h", "include/*/*.hpp"
+    no_copy_source = True
+    package_type = "header-library"
+
+    def source(self):
+        git = Git(self)
+        git.clone(
+            url="https://github.com/DEShawResearch/random123.git",
+            target=".",
+            args=["--branch", self.version, "--depth", "1"],
+        )
+
+    def package(self):
+        # This will also copy the "include" folder
+        copy(self, "include/*.h", self.source_folder, self.package_folder)
+        copy(self, "include/*.hpp", self.source_folder, self.package_folder)
+
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
