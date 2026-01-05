dda90ad5c8d22294d42f727d287c3e27c2b89796
Parantapa Bhattacharya <pb@parantapa.net>
2026-01-05 13:32:53 -0500

    Add helper script

---
+++ scripts/pb-dev.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+. "$HOME/default-env.sh"
+
+run_remove () {
+    # conan remove "random123" --confirm
+    # conan remove "gurobi" --confirm
+    # conan remove "ortools" --confirm
+
+    # conan remove "hpc-utils" --confirm
+
+    # conan remove "munge" --confirm
+
+    # conan remove "openpmix" --confirm
+    # conan remove "prrte" --confirm
+
+    # conan remove "openucx" --confirm
+    # conan remove "openucc" --confirm
+    # conan remove "openmpi" --confirm
+}
+
+run_install () {
+    # conan create -vverbose recipes/openmpi/all/ --version="5.0.9.pci" --options="openmpi/*:cuda=True" --build=missing
+}
+
+show_help() {
+    echo "Usage: $0 (help | command)"
+}
+
+if [[ "$1" == "help" || "$1" == "-h" || "$1" == "--help" ]]; then
+    show_help
+elif [[ $(type -t "run_${1}") == function ]]; then
+    run_${1}
+else
+    echo "Unknown command: $1"
+fi
