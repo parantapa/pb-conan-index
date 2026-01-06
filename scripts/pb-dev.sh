5eb7b86ca62d7c42471801aee1bcdfde8a978395
Parantapa Bhattacharya <pb@parantapa.net>
2026-01-06 17:33:25 -0500

    .

--- scripts/pb-dev.sh
+++ scripts/pb-dev.sh
@@ -7,7 +7,7 @@ run_remove () {
     # conan remove "gurobi" --confirm
     # conan remove "ortools" --confirm
 
-    # conan remove "hpc-utils" --confirm
+    conan remove "hpc-utils" --confirm
 
     # conan remove "munge" --confirm
     # conan remove "openpmix" --confirm
@@ -19,6 +19,8 @@ run_remove () {
 
 run_install () {
     # conan create -vverbose recipes/openmpi/all/ --version="5.0.9.pci" --options="openmpi/*:cuda=True" --build=missing
+    
+    conan create -vverbose recipes/hpc-utils/all/ --version="main" --build=missing
 }
 
 show_help() {
