dfeaca90b08dc6a3dc2f2d16fc81a7c40a0af49f
Parantapa Bhattacharya <pb@parantapa.net>
2026-02-19 10:10:34 -0500

    pb-dev script update

--- scripts/pb-dev.sh
+++ scripts/pb-dev.sh
@@ -5,21 +5,34 @@
 run_clean () {
     # conan remove "random123" --confirm
     # conan remove "gurobi" --confirm
-
-    conan remove "ortools" --confirm
+    # conan remove "ortools" --confirm
 
     # conan remove "munge" --confirm
     # conan remove "openpmix" --confirm
     # conan remove "prrte" --confirm
+
+    # conan remove "rdma-core" --confirm
+
     # conan remove "openucx" --confirm
     # conan remove "openucc" --confirm
     # conan remove "openmpi" --confirm
 }
 
 run_install () {
-    # conan create -vverbose recipes/openmpi/all/ --version="5.0.9.pci" --options="openmpi/*:cuda=True" --build=missing
+    # conan create recipes/munge/all/ --version="0.5.17.pci" -vverbose --build=missing
+    # conan create recipes/openpmix/all/ --version="5.0.9.pci" -vverbose --build=missing
+    # conan create recipes/prrte/all/ --version="3.0.12.pci" -vverbose --build=missing
 
-    conan create -vverbose recipes/ortools/all/ --version="9.15.pci" --build=missing
+    # conan create recipes/openucx/all/ --version="1.20.0.pci" -vverbose --build=missing \
+    #     --options="openucx/*:cuda=True" \
+    #     --options="openucx/*:rdma=True"
+    #
+    # conan create recipes/openucc/all/ --version="1.6.0.pci" -vverbose --build=missing \
+    #     --options="openucx/*:cuda=True" \
+    #     --options="openucx/*:rdma=True"
+
+    # conan create -vverbose recipes/openmpi/all/ --version="5.0.9.pci" --options="openmpi/*:cuda=True" --build=missing
+    # conan create -vverbose recipes/ortools/all/ --version="9.15.pci" --build=missing
 }
 
 show_help() {
